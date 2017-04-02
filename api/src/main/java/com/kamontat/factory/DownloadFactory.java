f2df281480f2fdbd83d2dd67305b6c00972aa765
kamontat <kamontat_c@hotmail.com>
2017-04-02 17:53:15 +0700

    More information in Downloadable

--- api/src/main/java/com/kamontat/factory/DownloadFactory.java
+++ api/src/main/java/com/kamontat/factory/DownloadFactory.java
@@ -1,9 +1,12 @@
 package com.kamontat.factory;
 
-import com.kamontat.api.FileManagement;
 import com.kamontat.exception.UpdateException;
 import com.kamontat.rawapi.Downloadable;
+import com.kamontat.utilities.URLManager;
+import com.kamontat.utilities.URLReader;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.Callable;
 
@@ -16,34 +19,48 @@ public class DownloadFactory {
 	private static DownloadFactory instance;
 	
 	private Downloadable download;
-	private static Callable<String> call;
 	
-	public static DownloadFactory getInstance() {
-		if (instance == null) instance = new DownloadFactory();
-		return instance;
-	}
-	
-	private DownloadFactory() {
-		download = () -> {
-			try {
-				return call.call();
-			} catch (Exception e) {
-				throw UpdateException.class.cast(e);
+	private DownloadFactory(URL link, Callable<String> action, long size, String content) {
+		download = new Downloadable() {
+			@Override
+			public String download() throws UpdateException {
+				try {
+					return action.call();
+				} catch (Exception e) {
+					throw new UpdateException(link, e);
+				}
+			}
+			
+			@Override
+			public long getSize() {
+				return size;
+			}
+			
+			@Override
+			public String getContentType() {
+				return content;
 			}
 		};
 	}
 	
-	public static void setLocation(URL link, String distDirectory) {
-		call = () -> {
-			try {
-				return FileManagement.getDownload(link, distDirectory).call();
-			} catch (Exception e) {
-				throw new UpdateException(link, e);
-			}
-		};
-	}
-	
-	public Downloadable getDownload() {
-		return download;
+	public static Downloadable setLocation(URL link, String distDirectory) {
+		try {
+			URLReader read = new URLReader(link, new File(distDirectory));
+			long size = read.getTotalByte();
+			String contentType = URLManager.getUrl(link).getConnection(URLManager.HTTP_CONNECTION).getContentType();
+			Callable<String> call = () -> {
+				read.setInput();
+				while (read.read() != -1) {
+					// System.out.println(read.getBytesRead());
+					read.read();
+				}
+				return read.getOutputFile();
+			};
+			
+			return new DownloadFactory(link, call, size, contentType).download;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
--- api/src/main/java/com/kamontat/rawapi/Downloadable.java
+++ api/src/main/java/com/kamontat/rawapi/Downloadable.java
@@ -10,5 +10,29 @@ import java.util.concurrent.Callable;
  * @since Mon 20/Mar/2017 - 10:38 PM
  */
 public interface Downloadable extends Callable<String> {
-	String call() throws UpdateException;
+	/**
+	 * try to download something (this is long long method) <br>
+	 * You might need to execute in in background
+	 *
+	 * @return result location
+	 * @throws UpdateException
+	 * 		Error occurred when try to download
+	 */
+	String download() throws UpdateException;
+	
+	long getSize();
+	
+	String getContentType();
+	
+	/**
+	 * same thing with {@link #download()} method
+	 *
+	 * @return result location
+	 * @throws Exception
+	 * 		when error occurred
+	 */
+	@Override
+	default String call() throws Exception {
+		return download();
+	}
 }
