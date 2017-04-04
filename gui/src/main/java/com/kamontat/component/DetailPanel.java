9b7a74e13625af43b6a02bcafcbed1bfc0113911
kamontat <kamontat_c@hotmail.com>
2017-04-04 22:29:49 +0700

    start implement gui

--- gui/src/main/java/com/kamontat/component/DetailPanel.java
+++ gui/src/main/java/com/kamontat/component/DetailPanel.java
@@ -1,5 +1,9 @@
 package com.kamontat.component;
 
+import com.kamontat.constance.ContentType;
+import com.kamontat.constance.Type;
+import com.kamontat.convert.Converter;
+import com.kamontat.exception.ConversionException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -7,24 +11,86 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.util.*;
+
+import static com.kamontat.component.DetailPanel.TextType.H;
+import static com.kamontat.component.DetailPanel.TextType.HTML;
+
 /**
  * @author kamontat
  * @version 1.0
  * @since Sun 26/Mar/2017 - 6:07 PM
  */
 public class DetailPanel extends Application {
-	public Scene initScene() {
-		String html = "<html><body><h1>Hello world!</h1><br><code>print(\"say hello\");</code></body></html>";
-		String md = "How *can* **i** do `this`";
+	private static final int SIZE = 2;
+	private String htmlText;
+	
+	// accept type
+	enum TextType {
+		H,
+		HTML,
+		M,
+		MD,
+		MARKDOWN
+	}
+	
+	private void initApp() {
+		if (checkParamSize()) {
+			TextType t;
+			if ((t = getType()) != null) {
+				if (t == H || t == HTML) htmlText = getString();
+				else try {
+					htmlText = Converter.by(Converter.Type.MD2HTML).convert(getString()).toString();
+				} catch (ConversionException e) {
+					throwException(new IllegalArgumentException(e));
+				}
+			} else {
+				throwException(new IllegalArgumentException("Invalid input type"));
+			}
+		} else {
+			throwException(new IllegalArgumentException("Invalid input size"));
+		}
+	}
+	
+	private Scene initScene() {
 		
-		// HTMLEditor editor = new HTMLEditor();
-		// editor.setHtmlText();
+		
+		// showing readme
 		WebView v = new WebView();
 		WebEngine engine = v.getEngine();
-		engine.loadContent(md, "text/markdown");
+		engine.loadContent(htmlText, ContentType.get(Type.TEXT, ContentType.HTML));
 		
 		StackPane pane = new StackPane(v);
 		return new Scene(pane);
+	}
+	
+	@Override
+	public void init() throws Exception {
+		initApp();
+		super.init();
+	}
+	
+	private String getParam(int index) {
+		return getParameters().getUnnamed().get(index);
+	}
+	
+	private boolean checkParamSize() {
+		return getParameters().getUnnamed().size() == SIZE;
+	}
+	
+	private TextType getType() {
+		for (TextType t : TextType.values()) {
+			if (getParam(0).toLowerCase(Locale.ENGLISH).contains(t.name().toLowerCase(Locale.ENGLISH))) return t;
+		}
+		return null;
+	}
+	
+	private String getString() {
+		return getParam(1);
+	}
+	
+	private void throwException(RuntimeException e) {
+		throw e != null ? e: new RuntimeException("Error Occurred");
 	}
 	
 	
@@ -32,5 +98,15 @@ public class DetailPanel extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setScene(initScene());
 		primaryStage.show();
+	}
+	
+	public static Scene setHtml(String htmlText) {
+		DetailPanel p = new DetailPanel();
+		p.htmlText = htmlText;
+		return p.initScene();
+	}
+	
+	public static void main(String[] args) {
+		DetailPanel.launch();
 	}
 }
---
+++ gui/src/main/java/com/kamontat/example/DetailUsage.java
@@ -0,0 +1,17 @@
+package com.kamontat.example;
+
+/**
+ * @author kamontat
+ * @version 1.0
+ * @since Tue 04/Apr/2017 - 10:14 PM
+ */
+public class DetailUsage {
+	public static void main(String[] args) {
+//		DetailPanel p = new DetailPanel(DetailPanel.TextType.HTML, "What **I** can *do* `this`?");
+//		JFrame f = new JFrame("Test page");
+//		f.setContentPane(JxAndSw.toSwing(p.initScene()));
+//		f.pack();
+//		f.setVisible(true);
+//		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+}
