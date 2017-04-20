7ad0c6bdd519ef94c3f6932f7a3cbb7ec88f905c
kamontat <kamontat_c@hotmail.com>
2017-04-20 10:09:32 +0700

    make old ui avaliable

--- gui/src/main/java/com/kamontat/ReleasePopup.java
+++ gui/src/main/java/com/kamontat/ReleasePopup.java
@@ -1,191 +1,196 @@
-//package com.kamontat;
-//
-//import com.kamontat.api.Updater;
-//import com.kamontat.config.Configuration;
-//import com.kamontat.utilities.DesktopUtil;
-//
-//import javax.swing.*;
-//import javax.swing.event.HyperlinkEvent;
-//import javax.swing.text.Element;
-//import java.awt.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.InputEvent;
-//import java.net.URL;
-//
-///**
-// * The popup that contains update description and downloader link.
-// * <p>
-// * Need to implement method: <br>
-// * <ol>
-// * <li> {@link #actionPerformed(ActionEvent)} - when user click download/update button</li>
-// * </ol>
-// * Might to implement method: <br>
-// * <ol>
-// * <li>{@link #click(InputEvent, Element, URL, String)}</li>
-// * <li>{@link #placeholder(InputEvent, Element, URL, String)}</li>
-// * <li>{@link #exitPlaceholder(InputEvent, Element, URL, String)}</li>
-// * <li>{@link #otherEvent(InputEvent, Element, URL, String)}</li>
-// * </ol>
-// * Helper Method: <br>
-// * <ol>
-// * <li>{@link #waiting()}</li>
-// * <li>{@link #done()}</li>
-// * <li>{@link #setTooltip(InputEvent, String)}</li>
-// * <li>{@link #removeTooltip(InputEvent)}</li>
-// * <li>{@link #open(URL)}</li>
-// * </ol>
-// *
-// * @author kamontat
-// * @version 1.0
-// * @since Tue 14/Mar/2017 - 2:27 PM
-// */
-//public abstract class ReleasePopup extends AbstractAction implements Runnable {
-//	private static JDialog dialog;
-//	protected Updater update;
-//
-//	/**
-//	 * Constructor to create popup <br>
-//	 *
-//	 * @param update
-//	 * 		The updater
-//	 */
-//	public ReleasePopup(Updater update) {
-//		this.update = update;
-//	}
-//
-//	private JPanel setPanel() {
-//		JPanel panel = new JPanel(new BorderLayout());
-//		// north
-//		// panel.add(new JLabel(String.format("version: %s -> %s", Updater.getCurrentVersion(), Updater.getRemoteVersion()), SwingConstants.CENTER), BorderLayout.NORTH);
-//		// center
-//		JEditorPane label = new JEditorPane(Configuration.INPUT_TYPE, update.getDescription());
-//		label.setEditable(false);
-//		label.setOpaque(false);
-//		label.addHyperlinkListener(e -> {
-//			// hold
-//			if (e.getEventType().equals(HyperlinkEvent.EventType.ENTERED)) {
-//				placeholder(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
-//				// out hold
-//			} else if (e.getEventType().equals(HyperlinkEvent.EventType.EXITED)) {
-//				exitPlaceholder(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
-//				// click
-//			} else if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
-//				click(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
-//			} else {
-//				otherEvent(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
-//			}
-//		});
-//		panel.add(label, BorderLayout.CENTER);
-//		// south
-//		JButton button = new JButton(Configuration.DOWNLOAD_BUTTON);
-//		button.addActionListener(this);
-//		panel.add(button, BorderLayout.SOUTH);
-//		return panel;
-//	}
-//
-//	/**
-//	 * the download button been click
-//	 *
-//	 * @param e
-//	 * 		action event
-//	 */
-//	@Override
-//	public void actionPerformed(ActionEvent e) {
-//		// downloadedAlert
-//		if (Configuration.HAS_ALERT) {
-//			int result = JOptionPane.showConfirmDialog(null, Configuration.ALERT_COMPLETE_MESSAGE, Configuration.ALERT_COMPLETE_TITLE, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-//		}
-//	}
-//
-//	protected void setTooltip(InputEvent event, String text) {
-//		JEditorPane.class.cast(event.getComponent()).setToolTipText(text);
-//	}
-//
-//	protected void removeTooltip(InputEvent event) {
-//		JEditorPane.class.cast(event.getComponent()).setToolTipText(null);
-//	}
-//
-//	protected void open(URL url) {
-//		DesktopUtil.get().browse(url);
-//	}
-//
-//	/**
-//	 * Need to implement
-//	 *
-//	 * @param event
-//	 * 		input event
-//	 * @param element
-//	 * 		element
-//	 * @param link
-//	 * 		url link
-//	 * @param description
-//	 * 		description or link
-//	 */
-//	protected void click(InputEvent event, Element element, URL link, String description) {
-//		open(link);
-//	}
-//
-//	/**
-//	 * Need to implement
-//	 *
-//	 * @param event
-//	 * 		input event
-//	 * @param element
-//	 * 		element
-//	 * @param link
-//	 * 		url link
-//	 * @param description
-//	 * 		description or link
-//	 */
-//	protected void placeholder(InputEvent event, Element element, URL link, String description) {
-//		setTooltip(event, description);
-//	}
-//
-//	/**
-//	 * Need to implement
-//	 *
-//	 * @param event
-//	 * 		input event
-//	 * @param element
-//	 * 		element
-//	 * @param link
-//	 * 		url link
-//	 * @param description
-//	 * 		description or link
-//	 */
-//	protected void exitPlaceholder(InputEvent event, Element element, URL link, String description) {
-//		removeTooltip(event);
-//	}
-//
-//	/**
-//	 * need to implement
-//	 *
-//	 * @param inputEvent
-//	 * 		input event
-//	 * @param sourceElement
-//	 * 		element
-//	 * @param url
-//	 * 		url link
-//	 * @param description
-//	 * 		description or link
-//	 */
-//	protected void otherEvent(InputEvent inputEvent, Element sourceElement, URL url, String description) {
-//	}
-//
-//	protected void waiting() {
-//		dialog.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-//	}
-//
-//	protected void done() {
-//		dialog.setCursor(Cursor.getDefaultCursor());
-//	}
-//
-//	@Override
-//	public void run() {
-//		dialog = new JDialog((Frame) null, "Update Page");
-//		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-//		dialog.setContentPane(setPanel());
-//		dialog.pack();
-//		dialog.setVisible(true);
-//	}
-//}
+package com.kamontat;
+
+import com.kamontat.exception.UpdateException;
+import com.kamontat.factory.UpdaterFactory;
+import com.kamontat.rawapi.Updatable;
+import com.kamontat.utilities.DesktopUtil;
+
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.text.Element;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.net.URL;
+
+/**
+ * The popup that contains update description and downloader link.
+ * <p>
+ * Need to implement method: <br>
+ * <ol>
+ * <li> {@link #actionPerformed(ActionEvent)} - when user click download/update button</li>
+ * </ol>
+ * Might to implement method: <br>
+ * <ol>
+ * <li>{@link #click(InputEvent, Element, URL, String)}</li>
+ * <li>{@link #placeholder(InputEvent, Element, URL, String)}</li>
+ * <li>{@link #exitPlaceholder(InputEvent, Element, URL, String)}</li>
+ * <li>{@link #otherEvent(InputEvent, Element, URL, String)}</li>
+ * </ol>
+ * Helper Method: <br>
+ * <ol>
+ * <li>{@link #waited()}</li>
+ * <li>{@link #done()}</li>
+ * <li>{@link #setTooltip(InputEvent, String)}</li>
+ * <li>{@link #removeTooltip(InputEvent)}</li>
+ * </ol>
+ *
+ * @author kamontat
+ * @version 1.0
+ * @since Tue 14/Mar/2017 - 2:27 PM
+ */
+public class ReleasePopup extends AbstractAction implements Runnable {
+	private static JDialog dialog;
+	protected UpdaterFactory update;
+	
+	/**
+	 * Constructor to create popup <br>
+	 *
+	 * @param update
+	 * 		The updater
+	 */
+	public ReleasePopup(Updatable update) {
+		this.update = UpdaterFactory.setUpdater(update);
+	}
+	
+	public ReleasePopup(UpdaterFactory update) {
+		this.update = update;
+	}
+	
+	private JPanel setPanel() {
+		JPanel panel = new JPanel(new BorderLayout());
+		// north
+		panel.add(new JLabel(String.format("version: %s -> %s", update.getCurrentVersion(), update.getRemoteVersion()), SwingConstants.CENTER), BorderLayout.NORTH);
+		// center
+		JEditorPane label = new JEditorPane("text/html", update.getDescription());
+		label.setEditable(false);
+		label.setOpaque(false);
+		label.addHyperlinkListener(e -> {
+			// hold
+			if (e.getEventType().equals(HyperlinkEvent.EventType.ENTERED)) {
+				placeholder(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
+				// out hold
+			} else if (e.getEventType().equals(HyperlinkEvent.EventType.EXITED)) {
+				exitPlaceholder(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
+				// click
+			} else if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+				click(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
+			} else {
+				otherEvent(e.getInputEvent(), e.getSourceElement(), e.getURL(), e.getDescription());
+			}
+		});
+		panel.add(label, BorderLayout.CENTER);
+		// south
+		JButton button = new JButton("update!");
+		button.addActionListener(this);
+		panel.add(button, BorderLayout.SOUTH);
+		return panel;
+	}
+	
+	/**
+	 * the download button been click
+	 *
+	 * @param e
+	 * 		action event
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		waited();
+		String s = "";
+		try {
+			s = update.download(null);
+		} catch (UpdateException e1) {
+			s = e1.getMessage();
+		}
+		done();
+		JOptionPane.showMessageDialog(null, s, "Message!", JOptionPane.PLAIN_MESSAGE);
+	}
+	
+	protected void setTooltip(InputEvent event, String text) {
+		JEditorPane.class.cast(event.getComponent()).setToolTipText(text);
+	}
+	
+	protected void removeTooltip(InputEvent event) {
+		JEditorPane.class.cast(event.getComponent()).setToolTipText(null);
+	}
+	
+	/**
+	 * Need to implement
+	 *
+	 * @param event
+	 * 		input event
+	 * @param element
+	 * 		element
+	 * @param link
+	 * 		url link
+	 * @param description
+	 * 		description or link
+	 */
+	private void click(InputEvent event, Element element, URL link, String description) {
+		DesktopUtil.get().browse(link);
+	}
+	
+	/**
+	 * Need to implement
+	 *
+	 * @param event
+	 * 		input event
+	 * @param element
+	 * 		element
+	 * @param link
+	 * 		url link
+	 * @param description
+	 * 		description or link
+	 */
+	protected void placeholder(InputEvent event, Element element, URL link, String description) {
+		setTooltip(event, description);
+	}
+	
+	/**
+	 * Need to implement
+	 *
+	 * @param event
+	 * 		input event
+	 * @param element
+	 * 		element
+	 * @param link
+	 * 		url link
+	 * @param description
+	 * 		description or link
+	 */
+	protected void exitPlaceholder(InputEvent event, Element element, URL link, String description) {
+		removeTooltip(event);
+	}
+	
+	/**
+	 * need to implement
+	 *
+	 * @param inputEvent
+	 * 		input event
+	 * @param sourceElement
+	 * 		element
+	 * @param url
+	 * 		url link
+	 * @param description
+	 * 		description or link
+	 */
+	protected void otherEvent(InputEvent inputEvent, Element sourceElement, URL url, String description) {
+	}
+	
+	private void waited() {
+		dialog.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+	}
+	
+	private void done() {
+		dialog.setCursor(Cursor.getDefaultCursor());
+	}
+	
+	@Override
+	public void run() {
+		dialog = new JDialog((Frame) null, "Update Page");
+		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		dialog.setContentPane(setPanel());
+		dialog.pack();
+		dialog.setVisible(true);
+	}
+}
--- gui/src/main/java/com/kamontat/example/DetailUsage.java
+++ gui/src/main/java/com/kamontat/example/DetailUsage.java
@@ -1,17 +1,35 @@
 package com.kamontat.example;
 
+import com.kamontat.ReleasePopup;
+import com.kamontat.exception.UpdateException;
+import com.kamontat.factory.UpdaterFactory;
+import com.kamontat.implementation.GUpdater;
+import com.kamontat.objects.Owner;
+import com.kamontat.rawapi.Updatable;
+
 /**
  * @author kamontat
  * @version 1.0
  * @since Tue 04/Apr/2017 - 10:14 PM
  */
 public class DetailUsage {
-	public static void main(String[] args) {
-//		DetailPanel p = new DetailPanel(DetailPanel.TextType.HTML, "What **I** can *do* `this`?");
-//		JFrame f = new JFrame("Test page");
-//		f.setContentPane(JxAndSw.toSwing(p.initScene()));
-//		f.pack();
-//		f.setVisible(true);
-//		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	public static void main(String[] args) throws UpdateException {
+		//		DetailPanel p = new DetailPanel(DetailPanel.TextType.HTML, "What **I** can *do* `this`?");
+		//		JFrame f = new JFrame("Test page");
+		//		f.setContentPane(JxAndSw.toSwing(p.initScene()));
+		//		f.pack();
+		//		f.setVisible(true);
+		//		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		Owner owner = new Owner("kamontat", "CheckIDNumber");
+		Updatable update = new GUpdater(owner, "v1.0.0", 1);
+		
+		UpdaterFactory factory = UpdaterFactory.setUpdater(update);
+		
+		factory.checkRelease();
+		
+		ReleasePopup p = new ReleasePopup(factory);
+		
+		p.run();
 	}
 }
