32206a600effb99ab8977563205173c83a8e8269
kamontat <kamontat_c@hotmail.com>
2017-04-30 17:25:41 +0700

    make popup release extend abs ui

--- gui/src/main/java/com/kamontat/ReleasePopup.java
+++ gui/src/main/java/com/kamontat/ReleasePopup.java
@@ -1,6 +1,6 @@
 package com.kamontat;
 
-import com.kamontat.component.DetailPanel;
+import com.kamontat.abstractui.AbstractUI;
 import com.kamontat.exception.UpdateException;
 import com.kamontat.factory.UpdaterFactory;
 import com.kamontat.rawapi.Updatable;
@@ -8,7 +8,6 @@ import com.kamontat.rawapi.Updatable;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.util.*;
 
 /**
  * The popup that contains update description and downloader link.
@@ -17,92 +16,37 @@ import java.util.*;
  * @version 2.0
  * @since Tue 14/Mar/2017 - 2:27 PM
  */
-public class ReleasePopup extends AbstractAction implements Runnable {
-	private static JDialog dialog;
-	private Point point;
-	private UpdaterFactory update;
+public class ReleasePopup extends AbstractUI {
 	
-	/**
-	 * Constructor to create popup <br>
-	 *
-	 * @param update
-	 * 		The updater
-	 */
 	public ReleasePopup(Updatable update) throws UpdateException {
-		this(update, null);
+		super(update);
 	}
 	
 	public ReleasePopup(UpdaterFactory update) throws UpdateException {
-		this(update, null);
-	}
-	
-	public ReleasePopup(Updatable update, Point point) throws UpdateException {
-		this(UpdaterFactory.setUpdater(update), point);
-	}
-	
-	public ReleasePopup(UpdaterFactory update, Point point) throws UpdateException {
-		this.point = point;
-		this.update = update;
-		update.checkRelease();
-	}
-	
-	private JPanel setPanel() {
-		JPanel panel = new JPanel(new BorderLayout());
-		// north
-		JPanel p = new JPanel(new BorderLayout());
-		if (!update.isLatest()) {
-			p.add(new JLabel(String.format("current: %-6s", update.getCurrentVersion()), SwingConstants.CENTER), BorderLayout.NORTH);
-			p.add(new JLabel(String.format("newest : %-6s", update.getRemoteVersion()), SwingConstants.CENTER), BorderLayout.SOUTH);
-		} else {
-			p.add(new JLabel("Up to date (" + update.getCurrentVersion() + ")", SwingConstants.CENTER), BorderLayout.CENTER);
-		}
-		
-		panel.add(p, BorderLayout.NORTH);
-		// center
-		panel.add(new DetailPanel(update.getDescription(), DetailPanel.TextType.HTML).init(), BorderLayout.CENTER);
-		// south
-		if (!update.isLatest()) {
-			JButton button = new JButton("update!");
-			button.addActionListener(this);
-			panel.add(button, BorderLayout.SOUTH);
-		}
-		return panel;
+		super(update);
 	}
 	
 	/**
-	 * the download button been click
-	 *
-	 * @param e
-	 * 		action event
+	 * before starting loading
 	 */
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		waited();
-		String s = "";
-		try {
-			s = update.download(null);
-		} catch (UpdateException e1) {
-			s = e1.getMessage();
-		}
-		done();
-		JOptionPane.showMessageDialog(null, s, "Message!", JOptionPane.PLAIN_MESSAGE);
-	}
-	
-	private void waited() {
+	public void preExecute() {
 		dialog.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 	}
 	
-	private void done() {
+	/**
+	 * after starting loading
+	 */
+	public void postExecute() {
 		dialog.setCursor(Cursor.getDefaultCursor());
 	}
 	
+	public void postExecute(Object obj) {
+		dialog.setCursor(Cursor.getDefaultCursor());
+		JOptionPane.showMessageDialog(super.window, obj.toString(), "Message!", JOptionPane.PLAIN_MESSAGE);
+	}
+	
 	@Override
-	public void run() {
-		dialog = new JDialog((Frame) null, "Update Page");
-		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		dialog.setContentPane(setPanel());
-		if (Objects.nonNull(point)) dialog.setLocation(point);
-		dialog.pack();
-		dialog.setVisible(true);
+	public void actionPerformed(ActionEvent e) {
+	
 	}
 }
